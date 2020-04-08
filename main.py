0c6a3e708f2c45339663d09f5b705213d1d6a107
unknown <nt2552@gmail.com>
2020-04-08 14:58:30 -0400

    Record prices and get highest prices and post to discord

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import discord
 import boto3.dynamodb
+from datetime import timedelta, datetime
+from pytz import timezone
 from boto3.dynamodb.conditions import Key
 
 dynamo_client = boto3.client('dynamodb',
                       region_name='us-east-2',
                       aws_access_key_id = open("dynamo_accesskey.txt", 'r').read(),
                       aws_secret_access_key = open("dynamo_secret.txt", 'r').read())
+
+resource = boto3.resource('dynamodb',
+                      region_name='us-east-2',
+                      aws_access_key_id = open("dynamo_accesskey.txt", 'r').read(),
+                      aws_secret_access_key = open("dynamo_secret.txt", 'r').read())
+
+table = resource.Table("stalks")
+
 
 client = discord.Client()
 
@@ -21,8 +31,51 @@ async def on_message(message):
         return
 
     if message.content.startswith('!turnip'):
-        await message.channel.send('Hello!')
+        fmt = "%Y-%m-%d %H:%M:%S %Z%z"
+        split = message.content.split()
+        if len(split) == 1:
+            return
+        else:
+            est = message.created_at.astimezone(timezone("America/New_York")) - timedelta(hours=4)
+            time_of_day = ""
+            if(est.hour < 12):
+                time_of_day = "morning"
+            else:
+                time_of_day = "afternoon"
+            day_of_week = est.strftime('%A').lower()
+            username = message.author.name
+            price = int(split[1])
 
+            table.put_item(
+                Item={
+                    "day_of_week": day_of_week,
+                    "username": username,
+                    "time_of_day": time_of_day,
+                    "price": price
+                }
+            )
+
+            await message.add_reaction("✅")
+
+    if message.content.startswith('!prices'):
+        est = message.created_at.astimezone(timezone("America/New_York")) - timedelta(hours=4)
+        day_of_week = est.strftime('%A').lower()
+        time_of_day = ''
+        if (est.hour < 12):
+            time_of_day = "morning"
+        else:
+            time_of_day = "afternoon"
+        response = table.query(KeyConditionExpression=Key("day_of_week").eq(day_of_week))
+        max_price = 0
+        max_user = ""
+
+        for r in response["Items"]:
+            if(r["price"] > max_price and r['time_of_day'] == time_of_day):
+                max_price = r["price"]
+                max_user = r['username']
+
+        res = "The highest turnip prices right now are " + str(max_price) + " bells from " + max_user
+        await message.channel.send(res)
 
 token = open("token.txt", 'r')
 
